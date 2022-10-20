38ef4e838c56ecfd2b780d7b2215438c7a3db06e
Jonata f <jonatafelixc@gmail.com>
2022-10-20 16:49:17 -0300

    font

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/empel_site/',///deletar em produção
-  server: {
+  server: {///deletar em produção
     host: true
   }
 })
