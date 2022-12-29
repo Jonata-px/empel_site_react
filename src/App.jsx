2b4484c102b22733be61790c1b478b9b65c491c0
Jonatã Felix <jonatafelixc@gmail.com>
2022-12-29 17:01:39 -0300

    produção

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,6 @@ function App() {
         <Footer/>
         
       </BrowserRouter>
-       
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/empel_site/',///deletar em produção
-  server: {///deletar em produção
-    host: true
-  }
+  // base: '/empel_site/',///deletar em produção
+  // server: {///deletar em produção
+  //   host: true
+  // }
 })
